04aee7a7730097ccb579c1892a489fe56445f849
Aji Suryawan <ajisuryawan@smkrus.sch.id>
2023-01-14 07:53:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kalkulator10pplg2/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.kalkulator10pplg2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //list widget yang akan dikenakan aksi
+    EditText txtUsername;
+    EditText txtPassword;
+    Button btnLogin;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // konekkan semua komponen dengan xml nya
+        txtUsername = (EditText) findViewById(R.id.txtUsername);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        btnLogin = (Button) findViewById(R.id.btnPassword);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = txtUsername.getText().toString();
+                String password = txtPassword.getText().toString();
+                if (username.equalsIgnoreCase("admin") &&
+                        password.equalsIgnoreCase("admin")){
+//                    Toast.makeText(getApplicationContext(), "Sukses Login", Toast.LENGTH_SHORT).show();
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Sukses")
+                            .setMessage("Login Sukses")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // Continue with delete operation
+                                }
+                            })
+
+                            // A null listener allows the button to dismiss the dialog and take no further action.
+                            .setNegativeButton(android.R.string.no, null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Gagal Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
