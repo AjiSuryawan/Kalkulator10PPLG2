4c90972093001a5de9f5bb2d0c4d505a18e4af7d
Aji Suryawan <ajisuryawan@smkrus.sch.id>
2023-01-28 08:10:19 +0700

    fix: [AS] splash screeen session

--- app/src/main/java/com/example/kalkulator10pplg2/MainActivity.java
+++ app/src/main/java/com/example/kalkulator10pplg2/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +15,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String SHARED_PREFS = "shared_prefs";
+
+    public static final String EMAIL_KEY = "email_key";
+
+    public static final String PASSWORD_KEY = "password_key";
+
+    SharedPreferences sharedpreferences;
+    String email, password;
 
     //list widget yang akan dikenakan aksi
     EditText txtUsername;
@@ -35,7 +46,18 @@ public class MainActivity extends AppCompatActivity {
                 String password = txtPassword.getText().toString();
                 if (username.equalsIgnoreCase("admin") &&
                         password.equalsIgnoreCase("admin")){
+                    sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                    // below two lines will put values for
+                    // email and password in shared preferences.
+                    editor.putString(EMAIL_KEY, txtUsername.getText().toString());
+                    editor.putString(PASSWORD_KEY, txtPassword.getText().toString());
+
+                    // to save our data with key and value.
+                    editor.apply();
                         startActivity(new Intent(MainActivity.this, Calculator.class));
+                        finish();
                 }else{
                     Toast.makeText(getApplicationContext(), "Gagal Login", Toast.LENGTH_SHORT).show();
                 }
