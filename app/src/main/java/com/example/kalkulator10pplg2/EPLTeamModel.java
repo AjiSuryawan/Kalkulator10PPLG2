641ff067b2bd87d2c85f71c4d1c73fc11490a224
Aji Suryawan <ajisuryawan@smkrus.sch.id>
2023-04-15 08:54:53 +0700

    fix: [AS] add pb and detail page

--- app/src/main/java/com/example/kalkulator10pplg2/EPLTeamModel.java
+++ app/src/main/java/com/example/kalkulator10pplg2/EPLTeamModel.java
@@ -1,9 +1,34 @@
 package com.example.kalkulator10pplg2;
 
-public class EPLTeamModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class EPLTeamModel implements Parcelable {
     private String teamName;
     private String stadiun;
     private String strTeamBadge;
+
+    protected EPLTeamModel(Parcel in) {
+        teamName = in.readString();
+        stadiun = in.readString();
+        strTeamBadge = in.readString();
+    }
+
+    EPLTeamModel(){
+
+    }
+
+    public static final Creator<EPLTeamModel> CREATOR = new Creator<EPLTeamModel>() {
+        @Override
+        public EPLTeamModel createFromParcel(Parcel in) {
+            return new EPLTeamModel(in);
+        }
+
+        @Override
+        public EPLTeamModel[] newArray(int size) {
+            return new EPLTeamModel[size];
+        }
+    };
 
     public String getTeamName() {
         return teamName;
@@ -27,5 +52,17 @@ public class EPLTeamModel {
 
     public void setStrTeamBadge(String strTeamBadge) {
         this.strTeamBadge = strTeamBadge;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(teamName);
+        parcel.writeString(stadiun);
+        parcel.writeString(strTeamBadge);
     }
 }
